db50fe0a98f5bce03077bc50350aa98c6aa43b2d
Luís Teixeira <luis.teixeira@arquiconsult.com>
2023-10-12 23:40:04 +0100

    Deployments

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -48,7 +48,7 @@ function Login() {
                     className="mb-4 grid h-28 place-items-center"
                 >
                     <Typography variant="h3" color="white">
-                        Sign In
+                        Welcome
                     </Typography>
                 </CardHeader>
                 <form onSubmit={handleLogin}>
